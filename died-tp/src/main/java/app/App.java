e31c202a681d338b1f3dd812d1b126506648ae5a
franp <franp@DESKTOP-4U0E43M>
2023-08-01 23:00:43 -0300

    Merge branch 'main' of https://github.com/qxtclxr/DIED-TP into main

--- died-tp/src/main/java/app/App.java
+++ died-tp/src/main/java/app/App.java
@@ -6,6 +6,7 @@ public class App {
 	public static void main(String[] args) {
 		try {
 			Ventana frame = new Ventana();
+			
 			frame.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
