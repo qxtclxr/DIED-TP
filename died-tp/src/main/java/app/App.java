f7f84b2f894948a8192e634bb0b67aac243cf42e
franp <franp@DESKTOP-4U0E43M>
2023-08-03 01:23:03 -0300

    Va el algoritmo de flujo maximo

--- died-tp/src/main/java/app/App.java
+++ died-tp/src/main/java/app/App.java
@@ -1,10 +1,20 @@
 package app;
 
+import java.sql.Time;
+import java.util.ArrayList;
+
+import datos.Operatividad;
+import datos.Ruta;
+import datos.Sucursal;
+import datos.TipoSucursal;
 import gui.*;
+import logica.GestorRuta;
+import logica.GestorSucursal;
+import logica.grafo.Grafo;
 
 public class App {
 	public static void main(String[] args) {
-		try {
+		try {				
 			Ventana frame = new Ventana();
 			frame.setVisible(true);
 		} catch (Exception e) {
